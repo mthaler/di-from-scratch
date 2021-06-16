8d2f515c7035dd6f01ed71bfe72303fb68bc8ebf
Michael Thaler <michael.thaler@dlr.de>
2021-06-16 10:53:08 +0200

    Added more comments.

--- src/main/kotlin/com/mthaler/difromscratch/Inject.kt
+++ src/main/kotlin/com/mthaler/difromscratch/Inject.kt
@@ -3,6 +3,9 @@ package com.mthaler.difromscratch
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
+/**
+ * Fields of a service that reference another service need to be annotated with this annotation
+ */
 @Target(AnnotationTarget.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 annotation class Inject
--- src/main/kotlin/com/mthaler/difromscratch/Service.kt
+++ src/main/kotlin/com/mthaler/difromscratch/Service.kt
@@ -3,6 +3,9 @@ package com.mthaler.difromscratch
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
+/**
+ * All services need to be annotated with the Service annotation.
+ */
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
 @Retention(RetentionPolicy.RUNTIME)
 annotation class Service
